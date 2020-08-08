90814cb3e8b62ba0ac3476de3addf90a829793a1
Bo Vilstrup Mortensen <bo.vilstrup@gmail.com>
2020-08-08 13:52:53 +0200

    made changes to package-lock.json and pachage.json and src/index.ts page 24

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const todoItem_1 = require("./todoItem");
 const todoCollection_1 = require("./todoCollection");
+const inquirer = require("inquirer");
 let todos = [
     new todoItem_1.TodoItem(1, "Buy Flowers"),
     new todoItem_1.TodoItem(2, "Get Shoes"),
@@ -9,11 +10,35 @@ let todos = [
     new todoItem_1.TodoItem(4, "Call joe", true)
 ];
 let collection = new todoCollection_1.TodoCollection("Adam", todos);
-console.clear();
-//console.log(`${collection.userName}'s Todo List`);
-//collection.removeComplete();
-console.log(`${collection.userName}'s Todo List ` + `(${collection.getItemCounts().imcomplete} items to do)`);
-collection.getTodoItems(true).forEach(item => item.printDetails());
-// let newId: number = collection.addTodo("Go for run");
-// let todoItem: TodoItem = collection.getTodoById(newId);
-// todoItem.printDetails();
+let showCompleted = true;
+function displayTodoList() {
+    console.log(`${collection.userName}'s Todo List `
+        + `(${collection.getItemCounts().imcomplete} items to do)`);
+    collection.getTodoItems(showCompleted).forEach(item => item.printDetails());
+}
+var Commands;
+(function (Commands) {
+    Commands["Toggle"] = "show/Hide completed";
+    Commands["Quit"] = "Quit";
+})(Commands || (Commands = {}));
+function promptUser() {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commands)
+        //badProperty: true
+    }).then(answers => {
+        switch (answers["command"]) {
+            case Commands.Toggle:
+                showCompleted = !showCompleted;
+                promptUser();
+                break;
+        } // End of switch()
+    });
+} // End of function promptUser()
+promptUser();
+// console.log(`${collection.userName}'s Todo List ` + `(${ collection.getItemCounts().imcomplete } items to do)`);
+// collection.getTodoItems(true).forEach(item => item.printDetails());
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { TodoItem } from './todoItem';
 import { TodoCollection } from './todoCollection';
+import * as inquirer from 'inquirer';
 
 let todos = [
     new TodoItem(1, "Buy Flowers"),
@@ -9,14 +10,39 @@ let todos = [
 ];
 
 let collection: TodoCollection = new TodoCollection("Adam", todos);
+let showCompleted = true;
+    
+function displayTodoList() : void {
+    console.log(`${collection.userName}'s Todo List `
+        + `(${ collection.getItemCounts().imcomplete } items to do)`);
+    collection.getTodoItems(showCompleted).forEach(item => item.printDetails());
+}
 
-console.clear();
-//console.log(`${collection.userName}'s Todo List`);
-//collection.removeComplete();
+enum Commands {
+    Toggle = "show/Hide completed",
+    Quit = "Quit"
+}
 
-console.log(`${collection.userName}'s Todo List ` + `(${ collection.getItemCounts().imcomplete } items to do)`);
-collection.getTodoItems(true).forEach(item => item.printDetails());
+function promptUser(): void {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commands)
+        //badProperty: true
+    }).then(answers => {
+        switch (answers["command"]) {
+            case Commands.Toggle:
+                showCompleted = !showCompleted;
+                promptUser();
+                break;
+        } // End of switch()
+    })
+} // End of function promptUser()
 
-// let newId: number = collection.addTodo("Go for run");
-// let todoItem: TodoItem = collection.getTodoById(newId);
-// todoItem.printDetails();
+promptUser();
+
+// console.log(`${collection.userName}'s Todo List ` + `(${ collection.getItemCounts().imcomplete } items to do)`);
+// collection.getTodoItems(true).forEach(item => item.printDetails());
